a002e4c9b0e71a658fa51c40fa64b4cc8e3b9cd9
Hanspeter Dietiker <hansdietiker2012@gmail.com>
2025-07-25 16:55:43 -0300

    feat:func sendEmail

---
+++ src/lib/notifier.rb
@@ -0,0 +1,28 @@
+require "uri"
+require "net/http"
+require "json"
+require "mail"
+
+require_relative "../config/settings"
+
+module Notifier
+  def self.send_email_alert(subject, body)
+    Mail.deliver do
+      from "contactdevhanspeter@gmail.com"
+      to "hansdietiker2012@gmail.com"
+      subject subject
+      body body
+      delivery_method :smtp, {
+        address: "seu.smtp.server ",
+        port: 587,
+        user_name: "test",
+        password: "123test",
+        authentication: "plain",
+        enable_starttls_auto: true,
+      }
+    end
+    puts "[EMAIL ALERT] #{subject}"
+  rescue StandartError => e
+    puts "Error to send email : #{e.message}"
+  end
+end
